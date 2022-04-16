4a4eafe41ae80f0aa6daa07cd965bba1a27ba188
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2022-04-16 10:08:44 -0400

    Update IngressTest.java

--- src/test/java/test/IngressTest.java
+++ src/test/java/test/IngressTest.java
@@ -91,7 +91,20 @@ class IngressTest {
                 .assertHasValue("SERVER_PROXY_PORT", "443")
                 .assertHasValue("SETUP_BASEURL", "https://myhost.mydomain");
     }
+    
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "bitbucket")
+    void bitbucket_ingress_host_port(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.host", "myhost.mydomain",
+                "ingress.port", "666"));
 
+        resources.getStatefulSet(product.getHelmReleaseName()).getContainer().getEnv()
+                .assertHasValue("SERVER_PROXY_NAME", "myhost.mydomain")
+                .assertHasValue("SERVER_PROXY_PORT", "666")
+                .assertHasValue("SETUP_BASEURL", "https://myhost.mydomain:666");
+    }
+    
     @ParameterizedTest
     @EnumSource(value = Product.class, names = {"bamboo_agent", "bitbucket"}, mode = EnumSource.Mode.EXCLUDE)
     void https_disabled(Product product) throws Exception {
